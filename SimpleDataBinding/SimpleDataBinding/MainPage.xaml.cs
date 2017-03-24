daff9d42b71865fff1ebe8a87b3ca9bb8ffeaf99
webmasterdevlin <webmasterdevlin@gmail.com>
2017-03-24 22:18:48 +0800

    Add project files.

---
+++ SimpleDataBinding/SimpleDataBinding/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var person = new Person("Devlin", "0920 545 4005", "devlin@xamarin.com");
+            BindingContext = person;
+            InitializeComponent();
+        }
+
+        private class Person
+        {
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+
+            public Person(string name, string phone, string email)
+            {
+                Name = name;
+                Phone = phone;
+                Email = email;
+            }
+        }
+    }
+}
